134ac68b7a5709828c4f0ac0c595586d4a595ded
Tomasz Bawor <bawortomasz@gmail.com>
2021-02-06 15:00:04 +0100

    Some content

--- src/components/GameContent/GameStats/index.tsx
+++ src/components/GameContent/GameStats/index.tsx
@@ -2,7 +2,24 @@ import React from "react";
 import "./GameStats.scss";
 
 const GameStats: React.FC = () => {
-  return <div className="stats">Stats</div>;
+  return (
+    <div className="stats">
+      <div className="stats--header">
+        <p>Stats</p>
+      </div>
+      <div className="stats--content">
+        <div>Money: 100.00$</div>
+      </div>
+
+      <div className="stats--header">
+        <p>Transactions</p>
+      </div>
+      <div className="stats--transactions">
+        <div className="stats-transaction">+100.01$</div>
+        <div className="stats-transaction">-100.01$</div>
+      </div>
+    </div>
+  );
 };
 
 export default GameStats;
